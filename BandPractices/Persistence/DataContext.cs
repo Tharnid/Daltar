4674f8d8dc57e0f95aa9aa3d006a72c42ff300f8
Tharnid <jeffmsmithdev@gmail.com>
2019-12-13 08:51:19 -0500

    saving progress so far

---
+++ BandPractices/Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public ClassName(Parameters)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
