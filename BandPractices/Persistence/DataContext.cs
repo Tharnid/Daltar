a8c9cc3d75efef615f47c886e0299e9fd77d6bdc
Tharnid <jeffmsmithdev@gmail.com>
2019-12-13 17:01:09 -0500

    on to client side

--- BandPractices/Persistence/DataContext.cs
+++ BandPractices/Persistence/DataContext.cs
@@ -6,11 +6,22 @@ namespace Persistence
 {
     public class DataContext : DbContext
     {
-        public ClassName(Parameters)
+        public DataContext(DbContextOptions options) : base(options)
         {
             
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { id = 1, Name = "Value 101" },
+                    new Value { id = 2, Name = "Value 102" },
+                    new Value { id = 3, Name = "Value 103" }
+                );
+        }
+
     }
 }
